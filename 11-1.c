04dff3dae7a9f9c2fe3f19b955cd90d69e8e3008
seohyeonH <113253709+seohyeonH@users.noreply.github.com>
2023-01-17 14:45:51 +0900

    Update and rename 11-1-1.c to 11-1.c

--- 11-1.c
+++ 11-1.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 
 int main(void) {
+	// 문제 1
 	int min, max;
 	int sum = 0;
 	int arr[5];
@@ -15,7 +16,11 @@ int main(void) {
 
 		sum += arr[i];
 	}
-
 	printf("최댓값: %d, 최솟값: %d, 총 합: %d", max, min, sum);
+	
+	// 문제 2
+	char arr[] = "Good time";
+	for (int i = 0; arr[i] != '\0'; i++) printf("%c", arr[i]);
+	
 	return 0;
 }
