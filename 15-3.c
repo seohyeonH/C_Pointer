f7e7d47f057418c88450e9488f06286cc580dce4
seohyeonH <113253709+seohyeonH@users.noreply.github.com>
2023-01-25 14:38:46 +0900

    Update 15-3.c

--- 15-3.c
+++ 15-3.c
@@ -5,17 +5,18 @@
 int main(void) {
 	int arr[LEN];
 	int num;
-	int odd = 0, even = LEN -1;
+	int odd = 0, even = LEN - 1;
 
+	printf("총 10개의 숫자 입력\n");
 	for (int i = 0; i < LEN; i++) {
-		printf("ÀÔ·Â: ");
+		printf("입력: ");
 		scanf("%d", &num);
 		
-		if (num % 2 == 0) arr[even--] = num;
-		else arr[odd++] = num;
+		if (num % 2 == 0) arr[even--] = num;    // 짝수인 경우
+		else arr[odd++] = num;			// 홀수인 경우
 	}
 
-	printf("¹è¿­ ¿ä¼ÒÀÇ Ãâ·Â: ");
+	printf("배열 요소의 출력: ");
 	for (int i = 0; i < LEN; i++) printf("%d ", arr[i]);
 	return 0;
 }
