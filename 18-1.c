9fd06393ecfc254782358dc38013c5f2a170e149
seohyeonH <113253709+seohyeonH@users.noreply.github.com>
2023-01-31 17:07:32 +0900

    Update 18-1.c

--- 18-1.c
+++ 18-1.c
@@ -1,14 +1,13 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#define SIZE 5
 
-void SimpleFuncOne(int *arr1, int *arr2);
-void SimpleFunTwo(int(*arr3)[4], int(*arr4)[4]);
-void ComplexFuncOne(int **arr1, int *(*arr2)[5]);
-void ComplexFuncTwo(int ***arr3, int ***(*arr4)[5]);
+void SimpleFuncOne(int *arr1, int *arr2);		// 문제 2
+void SimpleFunTwo(int(*arr3)[4], int(*arr4)[4]);	// 문제 2
+void ComplexFuncOne(int **arr1, int *(*arr2)[5]);	// 문제 3
+void ComplexFuncTwo(int ***arr3, int ***(*arr4)[5]);	// 문제 3
 
 int main(void) {
-	/* 18-1.2
+	// 18-1.2
 	int arr1[3];
 	int arr2[4];
 	int arr3[3][4];
@@ -16,10 +15,10 @@ int main(void) {
 
 	SimpleFuncOne(arr1, arr2);
 	SimpleFuncTwo(arr3, arr4);
-	*/
 
 
-	/* 18-1.3
+	
+	// 18-1.3
 	int* arr1[3];
 	int* arr2[3][5];
 	int** arr3[5];
@@ -27,18 +26,19 @@ int main(void) {
 
 	ComplexFuncOne(arr1, arr2);
 	ComplexFuncTwo(arr3, arr4);
-	*/
 
 
-	/* 18-1.4
+
+	// 18-1.4
 	int arr[3][2] = {{1, 2}, {3, 4}, {5, 6}};
-	printf("%d %d \n", arr[1][0], arr[0][1]);				 // 3, 2
-	printf("%d %d \n", *(arr[2] + 1), *(arr[1] + 1));		 // 6, 4
+	printf("%d %d \n", arr[1][0], arr[0][1]);		 // 3, 2
+	printf("%d %d \n", *(arr[2] + 1), *(arr[1] + 1));	 // 6, 4
 	printf("%d %d \n", (*(arr + 2))[0], (*(arr + 0))[1]);	 // 5, 2
-	printf("%d %d \n", **arr, *(*(arr + 0) + 0));			 // 1, 1
-	*/
+	printf("%d %d \n", **arr, *(*(arr + 0) + 0));		 // 1, 1
+	
 
 
+	// 18-1.5
 	int arr[2][2][2] = { 1, 2, 3, 4, 5, 6, 7, 8 };
 	printf("%d \n", arr[1][0][1]);
 	printf("%d \n", *(arr[1][0]) + 1);
