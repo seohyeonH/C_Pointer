7298ae428e02f9e0c62a1c4acb26ee5eba659868
seohyeonH <113253709+seohyeonH@users.noreply.github.com>
2023-02-07 14:11:52 +0900

    Update 20-6.c

--- 20-6.c
+++ 20-6.c
@@ -6,10 +6,10 @@
 void Game(int* game, int* strike, int* ball);
 
 int main(void) {
-	int i, j, count, strike, ball;
+	int i, j;
+	int count = 1, strike = 0, ball = 0;
 	int game[3];
 
-	count = 1; 
 	srand((int)time(NULL));
 	for (i = 0; i < 3; i++) {   // 값이 중복될 경우를 대비하여 한 번 확인하는 코드(16-17줄) 추가.
 		game[i] = (rand() % 10);
@@ -19,7 +19,6 @@ int main(void) {
 		
 	printf("Start Game!\n");
 	while (1) {
-		strike = 0, ball = 0;
 
 		Game(game, &strike, &ball);
 		printf("%d번째 도전 결과: %dstrike, %dball!!\n", count++, strike, ball);
@@ -32,7 +31,6 @@ int main(void) {
 
 void Game(int* game, int *strike, int *ball) {
 	int user[3];
-
 	printf("3개의 숫자 선택: ");
 	for (int i = 0; i < 3; i++) scanf("%d", &user[i]);
 
